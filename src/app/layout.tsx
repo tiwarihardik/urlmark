a643ad60f2b0824cfccbcae26c5610480b65defc
Hardik Tiwari <72031072+tiwarihardik@users.noreply.github.com>
2024-12-12 04:27:54 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import {name} from './page';
-
+let name = 'user';
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
